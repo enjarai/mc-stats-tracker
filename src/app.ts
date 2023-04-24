82153a9b06c3cb8b345b6bafc366f205881ce938
enjarai <enjarai123@gmail.com>
2023-04-24 22:26:03 +0200

    Revert initial database population for now

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ db.exec(`
   );
 `);
 
-const job = new CronJob({cronTime: queryCron, runOnInit: true, onTick: async () => {
+const job = new CronJob({cronTime: queryCron, onTick: async () => {
   const now = new Date();
   console.log(`⏰ Querying mod downloads at ${now.toTimeString()}`);
   const stmt = db.prepare('INSERT INTO stats VALUES (?, ?, ?, ?, ?, ?);');
